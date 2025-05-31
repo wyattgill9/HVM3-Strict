58f2a36f00e4246914229b39e811c598d3f92303
Wyatt Gill <101477459+wyattgill9@users.noreply.github.com>
2025-05-30 20:31:34 -0700

    Nix Flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "HVM3 flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+                haskell.compiler.ghc912
+                haskellPackages.cabal-install 
+                haskellPackages.haskell-language-server
+          ];
+
+          shellHook = ''
+            echo "Welcome to the Haskell-C dev shell"
+          '';
+        };
+
+      });
+}
